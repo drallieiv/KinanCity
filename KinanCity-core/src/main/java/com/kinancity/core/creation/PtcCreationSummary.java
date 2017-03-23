9190bc7cb87757592f46d5979b761f4c91643ab1
Adrien Nguyen <knabyss@gmail.com>
2017-03-24 00:04:58 +0100

    add execution sumary time

--- KinanCity-core/src/main/java/com/kinancity/core/creation/PtcCreationSummary.java
+++ KinanCity-core/src/main/java/com/kinancity/core/creation/PtcCreationSummary.java
@@ -1,25 +1,30 @@
 package com.kinancity.core.creation;
 
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PtcCreationSummary {
 
-	private static final String LOG_FORMAT = "%s/%s account created";
-	
+	private static final String LOG_FORMAT = "%s/%s account created in %s";
+
 	private List<PtcCreationResult> results;
-	
+
 	private String errorMsg;
+
+	// Total generation time in seconds
+	private long duration;
 
 	public PtcCreationSummary(String errorMsg) {
 		this.errorMsg = errorMsg;
 	}
-	
+
 	public PtcCreationSummary() {
 		results = new ArrayList<>();
 	}
 
-	public void add(PtcCreationResult result){
+	public void add(PtcCreationResult result) {
 		results.add(result);
 	}
 
@@ -32,9 +37,33 @@ public class PtcCreationSummary {
 	}
 
 	public String toString() {
-		if(errorMsg != null){
+		if (errorMsg != null) {
 			return errorMsg;
 		}
-		return String.format(LOG_FORMAT, getNbSuccess(), getNbCreations());
+
+		return String.format(LOG_FORMAT, getNbSuccess(), getNbCreations(), getTimeInText());
+	}
+
+	private String getTimeInText() {
+
+		StringBuilder sb = new StringBuilder();
+		long nbHours = Math.floorDiv(duration, 3600);
+		if (nbHours > 0) {
+			sb.append(nbHours).append("h ");
+		}
+
+		long nbMinutes = Math.floorDiv(duration % 3600, 60);
+		if (nbHours > 0) {
+			sb.append(nbMinutes).append("m ");
+		}
+
+		long nbSeconds = duration % 60;
+		sb.append(nbSeconds).append("s ");
+
+		return null;
+	}
+
+	public void setDuration(LocalTime startTime, LocalTime endTime) {
+		duration = ChronoUnit.SECONDS.between(startTime, endTime);
 	}
 }
