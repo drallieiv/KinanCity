9929b997fbe7f4a7866ee8c945a86c6870110911
Adrien Nguyen <knabyss@gmail.com>
2017-05-22 00:21:54 +0200

    fix: add username lenght check
    fixes #34

--- ptc-api/src/main/java/com/kinancity/api/PtcSession.java
+++ ptc-api/src/main/java/com/kinancity/api/PtcSession.java
@@ -59,7 +59,7 @@ public class PtcSession {
 	private String pathSignup = "/parents/sign-up";
 
 	private final String PTC_PWD_EXPREG = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[#?!@$%^&><+`*()\\-\\]])[A-Za-z0-9#?!@$%^&><+`*()\\-\\]]{8,50}$";
-	private final String PTC_USENAME_EXPREG = "^[a-zA-Z0-9]+$";
+	private final String PTC_USENAME_EXPREG = "^[a-zA-Z0-9]{4,15}$";
 
 	
 	private OkHttpClient client;
@@ -108,7 +108,7 @@ public class PtcSession {
 		}
 		
 		if (!Pattern.matches(PTC_USENAME_EXPREG, username)) {
-			logger.error("Invalid username '{}', The username contains illegal values.", username);
+			logger.error("Invalid username '{}', The username may contains illegal values and must be between 4 and 15 characters long", username);
 			return false;
 		}	
 		
