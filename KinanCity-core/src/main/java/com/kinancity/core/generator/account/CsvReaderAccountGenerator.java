af72f0516fcfbcf70de86998d9e17147cdf418bd
Dave Shoreman <dshoreman@users.noreply.github.com>
2017-06-01 23:25:32 +0100

    Allow setting dob and country from CSV (fixes #40)
    Although the CSV parser passes all named columns through to the builder,
    that method only applies username, email and password. In addition,
    PtcSession within the API module forcibly overrides any set values.
    
    This applies `dob` and `country` if they're set in the CSV, and updates
    the API module to use any value set in `account` VS hardcoded values.

--- KinanCity-core/src/main/java/com/kinancity/core/generator/account/CsvReaderAccountGenerator.java
+++ KinanCity-core/src/main/java/com/kinancity/core/generator/account/CsvReaderAccountGenerator.java
@@ -101,6 +101,15 @@ public class CsvReaderAccountGenerator extends ListAccountGenerator implements A
 		account.setUsername(fieldMap.get(CsvHeaderConstants.USERNAME));
 		account.setPassword(fieldMap.get(CsvHeaderConstants.PASSWORD));
 		account.setEmail(fieldMap.get(CsvHeaderConstants.EMAIL));
+
+		if (fieldMap.containsKey(CsvHeaderConstants.DOB)) {
+			account.setDob(fieldMap.get(CsvHeaderConstants.DOB));
+		}
+
+		if (fieldMap.containsKey(CsvHeaderConstants.COUNTRY)) {
+			account.setCountry(fieldMap.get(CsvHeaderConstants.COUNTRY));
+		}
+
 		return account;
 	}
 }
