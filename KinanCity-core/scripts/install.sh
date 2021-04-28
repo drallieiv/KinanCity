e45833eae74fa533322bc1641a71e802c63618ad
Adrien Nguyen <knabyss@gmail.com>
2021-04-28 19:38:47 +0200

    update build process

---
+++ KinanCity-core/scripts/install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -s https://api.github.com/repos/drallieiv/KinanCity/releases | grep "browser_download_url" | grep "KinanCity-core" | grep "jar" | head -n 1 | cut -d '"' -f 4 | xargs wget -O KinanCity-core.jar
