5aa4c3f6499c062375daa21eb3614d0ee80ab2ca
Adrien Nguyen <knabyss@gmail.com>
2021-08-24 10:08:32 +0200

    Update CatpchaSolutionDto.java

--- KinanCity-captcha-AntiCaptcha/src/main/java/com/kinancity/core/captcha/antiCaptcha/dto/CatpchaSolutionDto.java
+++ KinanCity-captcha-AntiCaptcha/src/main/java/com/kinancity/core/captcha/antiCaptcha/dto/CatpchaSolutionDto.java
@@ -1,10 +1,12 @@
 package com.kinancity.core.captcha.antiCaptcha.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CatpchaSolutionDto {
 	@JsonProperty("gRecaptchaResponse")
 	private String gRecaptchaResponse;
