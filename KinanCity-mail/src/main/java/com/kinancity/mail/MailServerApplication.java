100c3b2c9d62b0447276bec323cd0bd5dd6eebdd
Adrien Nguyen <knabyss@gmail.com>
2022-06-28 22:53:19 +0200

    fix reading system env var

--- KinanCity-mail/src/main/java/com/kinancity/mail/MailServerApplication.java
+++ KinanCity-mail/src/main/java/com/kinancity/mail/MailServerApplication.java
@@ -27,245 +27,243 @@ import com.kinancity.mail.tester.ThrottleTester;
 @Slf4j
 public class MailServerApplication {
 
-	private static final String CONFIG_FILE = "config.properties";
+    private static final String CONFIG_FILE = "config.properties";
 
-	private static Properties config;
+    private static Properties config;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		loadConfig();
+        loadConfig();
 
-		String mode = "direct";
+        String mode = "direct";
 
-		if (args.length > 0) {
-			mode = args[0].toLowerCase();
-		}
+        if (args.length > 0) {
+            mode = args[0].toLowerCase();
+        }
 
-		if (mode.equals("test")) {
-			log.info("Start in Tester Mode");
+        if (mode.equals("test")) {
+            log.info("Start in Tester Mode");
 
-			ThrottleTester tester = new ThrottleTester();
+            ThrottleTester tester = new ThrottleTester();
 
-			if (args.length > 1) {
-				tester.setDelay(Integer.parseInt(args[1]));
-			}
+            if (args.length > 1) {
+                tester.setDelay(Integer.parseInt(args[1]));
+            }
 
-			if (args.length > 2) {
-				tester.setErrorDelay(Integer.parseInt(args[2]));
-			}
+            if (args.length > 2) {
+                tester.setErrorDelay(Integer.parseInt(args[2]));
+            }
 
-			if (args.length > 3) {
-				tester.setPauseAfterNRequests(Integer.parseInt(args[3]));
-			}
+            if (args.length > 3) {
+                tester.setPauseAfterNRequests(Integer.parseInt(args[3]));
+            }
 
-			if (args.length > 4) {
-				tester.setDelayAfterNRequests(Integer.parseInt(args[4]));
-			}
+            if (args.length > 4) {
+                tester.setDelayAfterNRequests(Integer.parseInt(args[4]));
+            }
 
-			tester.start();
-		} else if (mode.equals("file")) {
+            tester.start();
+        } else if (mode.equals("file")) {
 
-			String filePath = "links.log";
-			if (args.length > 1) {
-				filePath = args[1];
-			}
+            String filePath = "links.log";
+            if (args.length > 1) {
+                filePath = args[1];
+            }
 
-			LinkActivator activator = getQueueLinkActivator();
-			EmailChanger mailChanger = getEmailChanger();
+            LinkActivator activator = getQueueLinkActivator();
+            EmailChanger mailChanger = getEmailChanger();
 
-			HybridActivator hybrid = new HybridActivator(activator, mailChanger);
-			hybrid.start();
+            HybridActivator hybrid = new HybridActivator(activator, mailChanger);
+            hybrid.start();
 
-			log.info("Started in file Mode");
-			SkippedFileProcessor processor = new SkippedFileProcessor(hybrid, filePath);
-			processor.process();
+            log.info("Started in file Mode");
+            SkippedFileProcessor processor = new SkippedFileProcessor(hybrid, filePath);
+            processor.process();
 
-		} else {
-			LinkActivator activator = getQueueLinkActivator();
-			EmailChanger emailChanger;
+        } else {
+            LinkActivator activator = getQueueLinkActivator();
+            EmailChanger emailChanger;
 
-			if (mode.equals("log")) {
-				log.info("Started in Log Mode");
-				activator = new ToFileLinkActivator();
-				emailChanger = new ToFileEmailChanger();
-			} else {
-				log.info("Started in Direct Mode");
-				emailChanger = getEmailChanger();
-			}
-			activator.start();
+            if (mode.equals("log")) {
+                log.info("Started in Log Mode");
+                activator = new ToFileLinkActivator();
+                emailChanger = new ToFileEmailChanger();
+            } else {
+                log.info("Started in Direct Mode");
+                emailChanger = getEmailChanger();
+            }
+            activator.start();
 
-			// Start Wiser server
-			KinanWiser wiser = new KinanWiser();
-			int port = Integer.parseInt(config.getProperty("port", "25"));
-			wiser.setPort(port);
-			wiser.setHostname(config.getProperty("hostname", ""));
+            // Start Wiser server
+            KinanWiser wiser = new KinanWiser();
+            int port = Integer.parseInt(config.getProperty("port", "25"));
+            wiser.setPort(port);
+            wiser.setHostname(config.getProperty("hostname", ""));
 
-			// Additional Setup
-			wiser.getServer().setSoftwareName("Kinan Mail Server");
-
-
-			log.info("SMTP server started on port {}", port);
-
-			String allowedDomains = config.getProperty("allowedDomains");
-			if(allowedDomains != null) {
-				log.info("Only accept emails for " + allowedDomains);
-				wiser.setAllowedDomain(allowedDomains);
-			}
-
-			KcMessageHandlerFactory handlerFactory = new KcMessageHandlerFactory(activator, emailChanger);
-			boolean disableDomainFilter = config.getProperty("disableDomainFilter", "false").equals("true");
-			if (disableDomainFilter) {
-				handlerFactory.setAcceptAllFrom(true);
-			}
-
-			if(activator != null ) {
-				log.info("Email Activation is Enabled");
-			}
-
-			if(emailChanger != null) {
-				log.info("Email Change is Enabled");
-			}
+            // Additional Setup
+            wiser.getServer().setSoftwareName("Kinan Mail Server");
 
 
-			wiser.getServer().setMessageHandlerFactory(handlerFactory);
-			wiser.start();
-		}
-	}
+            log.info("SMTP server started on port {}", port);
 
-	public static void loadConfig() {
-		config = new Properties();
+            String allowedDomains = config.getProperty("allowedDomains");
+            if (allowedDomains != null) {
+                log.info("Only accept emails for " + allowedDomains);
+                wiser.setAllowedDomain(allowedDomains);
+            }
 
-		try {
-			File configFile = new File(CONFIG_FILE);
-			if (!configFile.exists()) {
-				log.info("Missing configuration file " + CONFIG_FILE);
-				return;
-			}
-			InputStream in = new FileInputStream(configFile);
-			config.load(in);
-			in.close();
-		} catch (IOException e) {
-			log.info("Error loading configuration file " + CONFIG_FILE);
-		}
+            KcMessageHandlerFactory handlerFactory = new KcMessageHandlerFactory(activator, emailChanger);
+            boolean disableDomainFilter = config.getProperty("disableDomainFilter", "false").equals("true");
+            if (disableDomainFilter) {
+                handlerFactory.setAcceptAllFrom(true);
+            }
 
-    if (config.getProperty("proxy") == null) {
-      String proxyEnvConfig = System.getenv("proxy");
-      if (proxyEnvConfig != null) {
-        log.info("Use proxy config from SytemEnv variable : " + proxyEnvConfig);
-        config.setProperty("proxy", proxyEnvConfig);
-      }
+            if (activator != null) {
+                log.info("Email Activation is Enabled");
+            }
+
+            if (emailChanger != null) {
+                log.info("Email Change is Enabled");
+            }
+
+
+            wiser.getServer().setMessageHandlerFactory(handlerFactory);
+            wiser.start();
+        }
     }
 
-    
+    public static void loadConfig() {
+        config = new Properties();
 
-	}
+        if (config.getProperty("proxy") == null) {
+            String proxyEnvConfig = System.getenv("proxy");
+            if (proxyEnvConfig != null) {
+                log.info("Load proxy config from SytemEnv variable : " + proxyEnvConfig);
+                config.setProperty("proxy", proxyEnvConfig);
+            }
+        }
 
-	private static EmailChanger getEmailChanger() {
-		String isActive = config.getProperty("emailChanger.active");
-		if(isActive == null || isActive.equals("true")) {
-			PasswordProvider passwordProvider = PasswordProviderFactory.getPasswordProvider(config);
-			DirectEmailChanger emailChanger = new DirectEmailChanger(passwordProvider);
+        try {
+            File configFile = new File(CONFIG_FILE);
+            if (!configFile.exists()) {
+                log.info("Missing configuration file " + CONFIG_FILE);
+                return;
+            }
+            InputStream in = new FileInputStream(configFile);
+            config.load(in);
+            in.close();
+        } catch (IOException e) {
+            log.info("Error loading configuration file " + CONFIG_FILE);
+        }
 
-			String proxy = config.getProperty("proxy");
-			if (proxy != null) {
-				if (proxy.contains("|")) {
-					List<String> proxies = new LinkedList<String>(Arrays.asList(proxy.split(Pattern.quote("|"))));
-					String initialProxy = proxies.get(0);
-					HttpProxy httpProxy = HttpProxy.fromURI(initialProxy);
-					log.info("Mailchanger using proxy " + httpProxy);
-					emailChanger.setHttpProxy(httpProxy);
+    }
 
-					proxies.remove(0);
-					for (String backupProxyStr : proxies) {
-						HttpProxy backupProxy = HttpProxy.fromURI(backupProxyStr);
-						httpProxy.getOtherProxies().add(backupProxy);
-						log.info("Mailchanger with backup proxy " + backupProxy);
-					}
+    private static EmailChanger getEmailChanger() {
+        String isActive = config.getProperty("emailChanger.active");
+        if (isActive == null || isActive.equals("true")) {
+            PasswordProvider passwordProvider = PasswordProviderFactory.getPasswordProvider(config);
+            DirectEmailChanger emailChanger = new DirectEmailChanger(passwordProvider);
 
-				} else {
-					HttpProxy httpProxy = HttpProxy.fromURI(proxy);
-					log.info("Mailchanger using proxy " + httpProxy);
-					emailChanger.setHttpProxy(httpProxy);
-				}
-			}
-			return emailChanger;
-		} else {
-			return null;
-		}
-	}
+            String proxy = config.getProperty("proxy");
+            if (proxy != null) {
+                if (proxy.contains("|")) {
+                    List<String> proxies = new LinkedList<String>(Arrays.asList(proxy.split(Pattern.quote("|"))));
+                    String initialProxy = proxies.get(0);
+                    HttpProxy httpProxy = HttpProxy.fromURI(initialProxy);
+                    log.info("Mailchanger using proxy " + httpProxy);
+                    emailChanger.setHttpProxy(httpProxy);
 
-	public static LinkActivator getQueueLinkActivator() {
+                    proxies.remove(0);
+                    for (String backupProxyStr : proxies) {
+                        HttpProxy backupProxy = HttpProxy.fromURI(backupProxyStr);
+                        httpProxy.getOtherProxies().add(backupProxy);
+                        log.info("Mailchanger with backup proxy " + backupProxy);
+                    }
 
-		QueueLinkActivator activator;
+                } else {
+                    HttpProxy httpProxy = HttpProxy.fromURI(proxy);
+                    log.info("Mailchanger using proxy " + httpProxy);
+                    emailChanger.setHttpProxy(httpProxy);
+                }
+            }
+            return emailChanger;
+        } else {
+            return null;
+        }
+    }
 
-		String multithreadActive = config.getProperty("multithread.enable");
-		if (multithreadActive != null && !multithreadActive.equals("false")) {
-			MultiThreadQueueLinkActivator mtactivator = new MultiThreadQueueLinkActivator();
-			
-			String multithreadNbThread = config.getProperty("multithread.nbThread");
-			if(multithreadNbThread != null){
-				mtactivator.setNbThreads(Integer.parseInt(multithreadNbThread));	
-			}
-			activator = mtactivator;
-		} else {
-			activator = new QueueLinkActivator();
-		}
+    public static LinkActivator getQueueLinkActivator() {
 
-		String proxy = config.getProperty("proxy");
-		if (proxy != null) {
-			if (proxy.contains("|")) {
-				List<String> proxies = new LinkedList<String>(Arrays.asList(proxy.split(Pattern.quote("|"))));
-				String initialProxy = proxies.get(0);
-				HttpProxy httpProxy = HttpProxy.fromURI(initialProxy);
-				log.info("Activator using proxy " + httpProxy);
-				activator.setHttpProxy(httpProxy);
+        QueueLinkActivator activator;
 
-				proxies.remove(0);
-				for (String backupProxyStr : proxies) {
-					HttpProxy backupProxy = HttpProxy.fromURI(backupProxyStr);
-					httpProxy.getOtherProxies().add(backupProxy);
-					log.info("Activator with backup proxy " + backupProxy);
-				}
+        String multithreadActive = config.getProperty("multithread.enable");
+        if (multithreadActive != null && !multithreadActive.equals("false")) {
+            MultiThreadQueueLinkActivator mtactivator = new MultiThreadQueueLinkActivator();
 
-			} else {
-				HttpProxy httpProxy = HttpProxy.fromURI(proxy);
-				log.info("Activator using proxy " + httpProxy);
-				activator.setHttpProxy(httpProxy);
-			}
-		}
+            String multithreadNbThread = config.getProperty("multithread.nbThread");
+            if (multithreadNbThread != null) {
+                mtactivator.setNbThreads(Integer.parseInt(multithreadNbThread));
+            }
+            activator = mtactivator;
+        } else {
+            activator = new QueueLinkActivator();
+        }
 
-		String limiterActive = config.getProperty("limiter.enable");
-		if (limiterActive != null && !limiterActive.equals("false")) {
-			String period = config.getProperty("limiter.period");
-			String nb = config.getProperty("limiter.nb");
-			String pause = config.getProperty("limiter.pause");
+        String proxy = config.getProperty("proxy");
+        if (proxy != null) {
+            if (proxy.contains("|")) {
+                List<String> proxies = new LinkedList<String>(Arrays.asList(proxy.split(Pattern.quote("|"))));
+                String initialProxy = proxies.get(0);
+                HttpProxy httpProxy = HttpProxy.fromURI(initialProxy);
+                log.info("Activator using proxy " + httpProxy);
+                activator.setHttpProxy(httpProxy);
 
-			RateLimiter limiter = new RateLimiter();
-			if (period != null) {
-				limiter.setPeriodInSeconds(Integer.parseInt(period));
-			}
-			if (nb != null) {
-				limiter.setNbPerPeriod(Integer.parseInt(nb));
-			}
-			if (pause != null) {
-				limiter.setLimiterPause(Integer.parseInt(pause));
-			}
+                proxies.remove(0);
+                for (String backupProxyStr : proxies) {
+                    HttpProxy backupProxy = HttpProxy.fromURI(backupProxyStr);
+                    httpProxy.getOtherProxies().add(backupProxy);
+                    log.info("Activator with backup proxy " + backupProxy);
+                }
 
-			log.info("Using limiter " + limiter);
-			activator.setLimiter(limiter);
-		}
-		
-		String proxyBanActive = config.getProperty("proxy.banProxyOn403");
-		if (proxyBanActive != null && !proxyBanActive.equals("false")) {
-			activator.setBanProxyOn403(true);
-		}
-		
-		String alwaysSwitchActive = config.getProperty("proxy.switchProxyOnSuccess");
-		if (alwaysSwitchActive != null && !alwaysSwitchActive.equals("false")) {
-			activator.setSwitchProxyOnSuccess(true);
-		}
-		
-		return activator;
-	}
+            } else {
+                HttpProxy httpProxy = HttpProxy.fromURI(proxy);
+                log.info("Activator using proxy " + httpProxy);
+                activator.setHttpProxy(httpProxy);
+            }
+        }
+
+        String limiterActive = config.getProperty("limiter.enable");
+        if (limiterActive != null && !limiterActive.equals("false")) {
+            String period = config.getProperty("limiter.period");
+            String nb = config.getProperty("limiter.nb");
+            String pause = config.getProperty("limiter.pause");
+
+            RateLimiter limiter = new RateLimiter();
+            if (period != null) {
+                limiter.setPeriodInSeconds(Integer.parseInt(period));
+            }
+            if (nb != null) {
+                limiter.setNbPerPeriod(Integer.parseInt(nb));
+            }
+            if (pause != null) {
+                limiter.setLimiterPause(Integer.parseInt(pause));
+            }
+
+            log.info("Using limiter " + limiter);
+            activator.setLimiter(limiter);
+        }
+
+        String proxyBanActive = config.getProperty("proxy.banProxyOn403");
+        if (proxyBanActive != null && !proxyBanActive.equals("false")) {
+            activator.setBanProxyOn403(true);
+        }
+
+        String alwaysSwitchActive = config.getProperty("proxy.switchProxyOnSuccess");
+        if (alwaysSwitchActive != null && !alwaysSwitchActive.equals("false")) {
+            activator.setSwitchProxyOnSuccess(true);
+        }
+
+        return activator;
+    }
 
 }
