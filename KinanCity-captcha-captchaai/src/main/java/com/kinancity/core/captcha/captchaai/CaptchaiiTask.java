973cefd19bb9680f92960e1184ea67292f89fd6f
Adrien Nguyen <knabyss@gmail.com>
2023-04-14 18:53:47 +0200

    Add Captchaai/Capsolver app key

--- KinanCity-captcha-captchaai/src/main/java/com/kinancity/core/captcha/captchaai/CaptchaiiTask.java
+++ KinanCity-captcha-captchaai/src/main/java/com/kinancity/core/captcha/captchaai/CaptchaiiTask.java
@@ -1,6 +1,5 @@
 package com.kinancity.core.captcha.captchaai;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -9,6 +8,7 @@ import java.time.LocalDateTime;
 public class CaptchaiiTask {
     private int nbTries = 0;
     private String taskId;
+    private String appId = "F651F862-8811-421D-9172-80A95BDFB1D3";
     private LocalDateTime sentTime;
 
     public CaptchaiiTask(String taskId) {
